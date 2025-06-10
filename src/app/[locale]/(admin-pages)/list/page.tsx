93f7359470c0395f1c6b166185439805b084a33d
zengyj <zengyj@homeking365.com>
2025-06-10 16:29:22 +0800

    feature: list-table

--- src/app/[locale]/(admin-pages)/list/page.tsx
+++ src/app/[locale]/(admin-pages)/list/page.tsx
@@ -2,6 +2,9 @@ import ListButtons from './components/list-buttons'
 import ListTitle from './components/list-title'
 import ListProvider from './context/list-context'
 import ListDialogs from './components/list-dialogs'
+import DataTable from './components/data-table'
+import { columns } from './components/columns'
+import { tasks } from './data/tasks'
 
 export default function Page() {
   return (
@@ -9,6 +12,9 @@ export default function Page() {
       <div className='mb-2 flex flex-wrap items-center justify-between space-y-2 gap-x-4'>
         <ListTitle />
         <ListButtons />
+      </div>
+      <div className='-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-y-0 lg:space-x-12'>
+        <DataTable data={tasks} columns={columns} />
       </div>
       <ListDialogs />
     </ListProvider>
